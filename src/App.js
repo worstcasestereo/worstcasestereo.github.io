5eb2187d481faec2c19e2d1fc97d2613ba181004
Thomas VIAL <github@ifusio.com>
2019-08-11 19:48:57 +0200

    Prepared Video component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Events from './components/Events';
 import Header from './components/Header';
 import Intro from './components/Intro';
+import Video from './components/Video';
 import LatestAlbum from './components/LatestAlbum';
 import Biography from './components/Biography';
 import Contact from './components/Contact';
---
+++ src/components/Video.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Video extends Component {
+  render() {
+    return (
+      <section className="intro full-width jIntro" id="home">
+        <div id="intro-carousel" className="carousel slide carousel-fade" data-pause="true">
+          <div className="carousel-inner" role="listbox">
+
+            <div className="item active intro-fullscreen">
+              <iframe width="100%" height="100%" src="https://www.youtube.com/embed/zqsZen2KiBs?autoplay=1&controls=0&loop=1&rel=0&showinfo=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Video;
+
