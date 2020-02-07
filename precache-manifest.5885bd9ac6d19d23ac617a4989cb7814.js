b48a73aa206a352260d9c74effdc61c3598980ac
Thomas Vial <github@ifusio.com>
2020-02-07 20:54:51 +0100

    Updates

---
+++ precache-manifest.5885bd9ac6d19d23ac617a4989cb7814.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f1bfc3f13ddacaa539145e98ecbf8cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0d884a555bcab14e5b4",
+    "url": "/static/css/main.f7829086.chunk.css"
+  },
+  {
+    "revision": "9701da1cf32b47289280",
+    "url": "/static/js/2.01147f45.chunk.js"
+  },
+  {
+    "revision": "7bc9b478038c6bedf8340d67bd512bd3",
+    "url": "/static/js/2.01147f45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0d884a555bcab14e5b4",
+    "url": "/static/js/main.c67a9e79.chunk.js"
+  },
+  {
+    "revision": "6147f4c70e269d5f5e9f",
+    "url": "/static/js/runtime-main.987d1c63.js"
+  }
+]);
